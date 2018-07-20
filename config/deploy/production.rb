a86b8148c83a725e7822ca6ecd59ffd04a23f6b3
Unbxd <raudra.pratap@unbxd.com>
2018-07-21 02:03:18 +0530

    sever config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+server '18.236.82.169', user: 'deploy', roles: %w{web app db}
 
 
 # role-based syntax
